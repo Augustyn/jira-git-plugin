5b2990e869925462d48f4e44769b38fb9b3afda3
stailgot <sanchesz@list.ru>
2016-04-28 03:14:38 +0300

    fix up

--- src/main/java/com/xiplink/jira/git/issuetabpanels/changes/GitRevisionsTabPanel.java
+++ src/main/java/com/xiplink/jira/git/issuetabpanels/changes/GitRevisionsTabPanel.java
@@ -6,11 +6,19 @@
 package com.xiplink.jira.git.issuetabpanels.changes;
 
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.action.IssueActionComparator;
 import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.plugin.issuetabpanel.IssueAction;
 import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
 import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanelModuleDescriptor;
+import com.atlassian.jira.security.PermissionManager;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.util.EasyList;
+import com.xiplink.jira.git.MultipleGitRepositoryManager;
+import com.xiplink.jira.git.revisions.RevisionIndexer;
+import com.xiplink.jira.git.revisions.RevisionInfo;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,63 +27,64 @@ public class GitRevisionsTabPanel implements IssueTabPanel
 {
 //    private static Logger log = Logger.getLogger(GitRevisionsTabPanel.class);
 
-//    protected final MultipleGitRepositoryManager multipleGitRepositoryManager;
-//    private PermissionManager permissionManager;
-//
-//    public GitRevisionsTabPanel(MultipleGitRepositoryManager multipleGitRepositoryManager, PermissionManager permissionManager)
-//    {
-//        this.multipleGitRepositoryManager = multipleGitRepositoryManager;
-//        this.permissionManager = permissionManager;
-//    }
+    protected final MultipleGitRepositoryManager multipleGitRepositoryManager;
+    private PermissionManager permissionManager;
+    private IssueTabPanelModuleDescriptor descriptor;
+
+    public GitRevisionsTabPanel(MultipleGitRepositoryManager multipleGitRepositoryManager, PermissionManager permissionManager)
+    {
+        this.multipleGitRepositoryManager = multipleGitRepositoryManager;
+        this.permissionManager = permissionManager;
+    }
 
     public void init(IssueTabPanelModuleDescriptor issueTabPanelModuleDescriptor)
     {
-
+        descriptor = issueTabPanelModuleDescriptor;
     }
 
     public List getActions(Issue issue, ApplicationUser remoteUser)
     {
-//        try
-//        {
-//            RevisionIndexer revisionIndexer = multipleGitRepositoryManager.getRevisionIndexer();
-//
-//            revisionIndexer.updateIndex();
-//            List<RevisionInfo> logEntries = revisionIndexer.getLogEntriesByRepository(issue);
-//
-//            // This is a bit of a hack to get the error message across
-//            if (logEntries == null)
-//            {
-//                GenericMessageAction action = new GenericMessageAction(getText("no.index.error.message"));
-//                return EasyList.build(action);
-//            } else if (logEntries.size() == 0)
-//            {
-//                GenericMessageAction action = new GenericMessageAction(getText("no.log.entries.message"));
-//                return EasyList.build(action);
-//            } else
-//            {
-//                List<IssueAction> actions = new ArrayList<IssueAction>(logEntries.size());
-//                for (RevisionInfo entry : logEntries)
-//                {
-//                    actions.add(new GitRevisionAction(entry.getCommit(), multipleGitRepositoryManager,
-//                            descriptor, entry.getRepositoryId(), entry.getBranch()));
-//                }
-//                Collections.sort(actions, IssueActionComparator.COMPARATOR);
-//                return actions;
-//            }
-//        } catch (Throwable t)
-//        {
+        try
+        {
+            RevisionIndexer revisionIndexer = multipleGitRepositoryManager.getRevisionIndexer();
+
+            revisionIndexer.updateIndex();
+            List<RevisionInfo> logEntries = revisionIndexer.getLogEntriesByRepository(issue);
+
+            // This is a bit of a hack to get the error message across
+            if (logEntries == null)
+            {
+                GenericMessageAction action = new GenericMessageAction(getText("no.index.error.message"));
+                return EasyList.build(action);
+            } else if (logEntries.size() == 0)
+            {
+                GenericMessageAction action = new GenericMessageAction(getText("no.log.entries.message"));
+                return EasyList.build(action);
+            } else
+            {
+                List<IssueAction> actions = new ArrayList<IssueAction>(logEntries.size());
+                for (RevisionInfo entry : logEntries)
+                {
+                    actions.add(new GitRevisionAction(entry.getCommit(), multipleGitRepositoryManager,
+                            descriptor, entry.getRepositoryId(), entry.getBranch()));
+                }
+                Collections.sort(actions, IssueActionComparator.COMPARATOR);
+                return actions;
+            }
+        } catch (Throwable t)
+        {
 //            log.error("Error retrieving actions for : " + issue.getKey(), t);
-//        }
+        }
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Nothing else maters");
         return Collections.singletonList(new GenericMessageAction(stringBuilder.toString()));
     }
 
-//    private String getText(String key)
-//    {
-//        return descriptor.getI18nBean().getText(key);
-//    }
+    private String getText(String key)
+    {
+        return descriptor.getI18nBean().getText(key);
+    }
 
     public boolean showPanel(Issue issue, ApplicationUser remoteUser)
     {
